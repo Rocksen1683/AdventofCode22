413673a16ef370cd360ec6101dd62a8c4678ff81
Ekanshh Praveen <edpravee@uwaterloo.ca>
2022-12-23 14:22:32 -0500

    Input for Day One

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -2,17 +2,18 @@
 with open('C:\Computer Science\AdventofCode22\Day1\input.txt') as f:
     lines = f.readlines()
 
-    maxNum = 0
     sum = 0
+    sums = []
 
     for line in lines:
         if line == '\n':
             #print("hi")
-            if sum > maxNum:
-                maxNum = sum
+            sums.append(sum)
             sum = 0
         else:
             print(line)
-            sum += int(line[0:len(line)-2])
-    
-    print(maxNum)
+            sum += int(line)
+    sums.sort()
+    print(sums)
+
+    #the answer would be the element at index -1 in the list [highest calories]
